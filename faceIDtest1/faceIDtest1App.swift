b57eb5086b688b436a92fa0faaeb4d954bfb0d6d
Qiwei <ql@qiweis-mbp.lan>
2022-09-21 23:14:55 -0400

    first commit

---
+++ faceIDtest1/faceIDtest1App.swift
@@ -0,0 +1,17 @@
+//
+//  faceIDtest1App.swift
+//  faceIDtest1
+//
+//  Created by Qiwei on 9/21/22.
+//
+
+import SwiftUI
+
+@main
+struct faceIDtest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
